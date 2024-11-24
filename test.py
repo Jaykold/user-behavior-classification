1310beeccf2ef656f43e612243dbd88f190014f9
Christian <christianichebi@gmail.com>
2024-11-25 00:45:30 +0100

    CHORE: Add requirements.txt and flask endpoint

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import requests
-from predict import predict
 from icecream import ic
 
 url = "http://127.0.0.1:9696/predict"
@@ -7,19 +6,17 @@ url = "http://127.0.0.1:9696/predict"
 user_id = "vbj-321"
 
 user_data = {
-    "device": "Xiaomi Mi 11",
+    "device": "Google Pixel 5",
     "operating_system":	"Android",
-    "app_usage_time(hours/day)": 154,
-    "screen_on_time(hours/day)": 4.00,
-    "battery_drain(mAh/day)": 761,
-    "number_of_apps_installed":	32,
-    "data_usage(mb/day)": 322,
-    "age": 42, 
+    "app_usage_time(hours/day)": 393,
+    "screen_on_time(hours/day)": 6.40,
+    "battery_drain(mAh/day)": 1331,
+    "number_of_apps_installed":	62,
+    "data_usage(mb/day)": 944,
+    "age": 25, 
     "gender": "Male"
 }
 
-#response = requests.post(url, json=user_data, timeout=5)
+response = requests.post(url, json=user_data, timeout=5).json()
 
-if __name__ == "__main__":
-    result = predict(user_data)
-    ic(result)
+ic(response)
---
+++ user_classifier.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from predict import predict
+import waitress
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict_user():
+    # Get the data from the POST request
+    user_data = request.get_json()
+    
+    # Make the prediction
+    prediction = predict(user_data)
+
+    return jsonify({'prediction': prediction})
+
+if __name__ == '__main__':
+    app.debug = True
+    app.config['ENV'] = 'development'
+    waitress.serve(app, port=9696, host='0.0.0.0')
